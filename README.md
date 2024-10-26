168d2fcaba843b58f57224138c46ac3dd060c415
Mathieu Geukens <mr-zwets@protonmail.com>
2024-10-26 17:26:25 +0200

    add templating logic

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ## Difficulties
 
 - CashScript doesn't currently support the `<withdraw_pkh>` templated variables in the middle of the contract code
--> workaround: manually replace the template string in the Artifact's `bytecode`
+-> solved: replace the template string in the Artifact's `bytecode` before initalizing contract
 
 - How to find all Cauldron contracts when they are all at unique addresses?
--- index.ts
+++ index.ts
@@ -1,16 +1,21 @@
-import { Contract, ElectrumNetworkProvider, TransactionBuilder, Utxo } from 'cashscript';
+import { Artifact, Contract, ElectrumNetworkProvider, Utxo } from 'cashscript';
 // The cauldronArtifact contains a template variable <withdraw_pkh>
-import cauldronArtifact from './artifact.json' with { type: 'json' }; 
+import cauldronArtifact from './artifact.json' with { type: 'json' };
+
+function cauldronContractWithPkh(pkhHex:string){
+  const strigifiedCauldronArtifact = JSON.stringify(cauldronArtifact);
+  return JSON.parse(strigifiedCauldronArtifact.replace('<withdraw_pkh>', pkhHex)) as Artifact
+}
 
 const provider = new ElectrumNetworkProvider('mainnet');
 const options = { provider, addressType:'p2sh32' as const };
-const cauldronContract = new Contract(cauldronArtifact, [], options);
+const pkhHex = "0204ccd5b8cdecbd68083a16284b5d2b9af1c4138250d548660c2c5c3b514f4ca5";
+const cauldronContract = new Contract(cauldronContractWithPkh(pkhHex), [], options);
 
-console.log('Cauldron contract address:', cauldronContract.address);
+console.log(`Cauldron contract address for pkh${pkhHex}:`, cauldronContract.address);
 
 export async function getCauldronUtxos(tokenId:string){
-  const allCauldronUtxos = await cauldronContract.getUtxos();
-  return allCauldronUtxos.filter(utxo => utxo?.token?.category === tokenId); 
+  // How to find all Cauldron contracts when they are all at unique addresses? 
 }
 
 export async function parsePriceCauldronUtxos(utxos:Utxo[]){
@@ -20,5 +25,3 @@ export async function parsePriceCauldronUtxos(utxos:Utxo[]){
     return { price: priceSatsPerToken, utxo };
   })
 }
-
-const transactionBuilder = new TransactionBuilder({ provider });
