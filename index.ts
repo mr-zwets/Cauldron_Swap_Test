6c46a7a1d1f078d713d24bc73a5e3a4e4ac17c25
Mathieu Geukens <mr-zwets@protonmail.com>
2024-10-27 09:00:42 +0100

    add user inputs & outputs to buyTokensPool

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
-import { Artifact, Contract, ElectrumNetworkProvider, Output, TransactionBuilder } from 'cashscript';
+import {
+  Contract,
+  ElectrumNetworkProvider,
+  Output,
+  SignatureTemplate,
+  TransactionBuilder
+} from 'cashscript';
 // The cauldronArtifact contains a template variable <withdraw_pkh>
-import cauldronArtifact from './artifact.json' with { type: 'json' };
 import { CauldronActivePool, CauldronGetActivePools } from './interfaces';
-import { convertPoolToUtxo } from './utils';
+import { cauldronContractWithPkh, convertPoolToUtxo } from './utils';
 
 const provider = new ElectrumNetworkProvider('mainnet');
-
-export function cauldronContractWithPkh(pkhHex:string){
-  const strigifiedCauldronArtifact = JSON.stringify(cauldronArtifact);
-  return JSON.parse(strigifiedCauldronArtifact.replace('<withdraw_pkh>', pkhHex)) as Artifact
-}
 
 export async function getCauldronUtxos(tokenId:string){ 
   const result = await fetch(`https://indexer.cauldron.quest/cauldron/pool/active?token=${tokenId}`)
@@ -28,7 +28,8 @@ export async function parsePoolPrices(pools:CauldronActivePool[]){
 export async function buyTokensPool(
   pool:CauldronActivePool,
   amountToBuy:number,
-  userAddress:string
+  userAddress:string,
+  privateKeyWif:string
 ){
   const cauldronUtxo = convertPoolToUtxo(pool);
   const UserUtxos = await provider.getUtxos(userAddress);
@@ -43,10 +44,26 @@ export async function buyTokensPool(
   const cauldronContract = new Contract(cauldronContractWithPkh(pool.owner_pkh), [], options);
   transactionBuilder.addInput(cauldronUtxo, cauldronContract.unlock.swap())
 
+  // calculate tradeValue and poolFee
   const poolConstant = pool.tokens * pool.sats
   const cauldronAmountExludingFee = Math.ceil(poolConstant / (pool.tokens - amountToBuy))
   const tradeValue = Math.abs(cauldronAmountExludingFee - pool.sats)
   const poolFee = 0.3 * tradeValue
+
+  // calculate user input amount
+  const userInputAmountNeeded = BigInt(tradeValue + poolFee) + 1000n
+  if(userBalanceSats < userInputAmountNeeded) throw new Error('Insufficient funds to buy tokens')
+  const sortedUserUtxos = UserUtxos.sort((a, b) => Number(a.satoshis) - Number(b.satoshis))
+
+  // add needed userInputs to the transactionBuilder
+  const userTemplate = new SignatureTemplate(privateKeyWif)
+  let userSatsInInputs = 0n
+  for(const userUtxo of sortedUserUtxos){
+    userSatsInInputs += userUtxo.satoshis
+    transactionBuilder.addInput(userUtxo, userTemplate.unlockP2PKH())
+    if(userSatsInInputs >= userInputAmountNeeded) break
+  }
+
   const newCauldronAmountSats = cauldronAmountExludingFee + poolFee
   const newCauldronAmountTokens = poolConstant / newCauldronAmountSats
 
@@ -70,5 +87,11 @@ export async function buyTokensPool(
   }
   transactionBuilder.addOutput(boughtTokensOutput)
 
-  // TODO: add user inputs and change output
+  const userChangeOutput: Output = {
+    to: userAddress,
+    amount: userSatsInInputs - userInputAmountNeeded,
+  }
+  transactionBuilder.addOutput(userChangeOutput)
+
+  return await transactionBuilder.send()
 }
--- utils.ts
+++ utils.ts
@@ -1,5 +1,11 @@
-import { type Utxo } from 'cashscript';
+import type { Artifact, Utxo } from 'cashscript';
+import cauldronArtifact from './artifact.json' with { type: 'json' };
 import { CauldronActivePool } from './interfaces';
+
+export function cauldronContractWithPkh(pkhHex:string){
+  const strigifiedCauldronArtifact = JSON.stringify(cauldronArtifact);
+  return JSON.parse(strigifiedCauldronArtifact.replace('<withdraw_pkh>', pkhHex)) as Artifact
+}
 
 export function convertPoolToUtxo(pool: CauldronActivePool):Utxo{
   return {
